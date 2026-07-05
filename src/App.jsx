ae10352afb2117c178ab2309471dfd1b0b5f5e34
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-05 19:09:10 +0530

    Creating Browser Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import NavBar from "./NavBar";
+
 function App(){
     return(
     <>
-    <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">devTinder👨🏻‍💻</a>
-  </div>
-  <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-900 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+    <BrowserRouter basename="/">
+    <Routes>
+        <Route path="/"element={<div>Base page</div>}/>
+        <Route path="/login" element={<div>Login page</div>}/>
+        <Route path="/test" element={<div>Test page</div>}/>
+
+    </Routes>
+    
+    
+    </BrowserRouter>
+
+    <NavBar/>
+ 
 
 
 
