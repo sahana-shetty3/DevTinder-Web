8e2556ca8cf36959a2c8e9433fcaebe82baf3ecc
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-06 11:15:49 +0530

    Creating login page and connect to backend through login api

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,48 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login =()=>{
+    const [emailId,setEmailId]= useState("");
+    const [password,setPassword]=useState("");
+
+    const handleLogin= async ()=>{
+    try{
+        const res= await axios.post("http://localhost:3/login",{
+            emailId,
+            password,
+        },{withCredentials:true}
+    );
+    }catch(err){
+        console.log(err);
+    };
+
+
+    }
+
     return(
-        <div>Login page  </div>)
+    <div className=" flex justify-center my-10 ">
+     <div className="card bg-base-300 w-96 shadow-xl">
+      <div className="card-body">
+       <h2 className="card-title justify-center">Login</h2>
+        <div>
+            <fieldset className="fieldset">
+                <legend className="fieldset-legend "> Email Id</legend>
+                <input type="text" value={emailId} className="input" onChange={(e)=> setEmailId(e.target.value)} />
+            </fieldset>
+        </div>
+        <div>
+            <fieldset className="fieldset">
+                <legend className="fieldset-legend "> Password </legend>
+                <input type="text" value={password} className="input" onChange={(e)=>setPassword(e.target.value)}/>
+            </fieldset>  
+        </div>
+        
+        <div className="card-actions justify-center">
+         <button className="btn btn-primary"onClick={handleLogin} >Login</button>
+       </div>
+      </div>
+     </div>
+    </div>)
     
 };
 export default Login;
