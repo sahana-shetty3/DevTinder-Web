a969e76c0f5348234eba6f8deec5e79674838ecf
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-08 12:21:52 +0530

    redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import NavBar from "./NavBar";
 import Login from "./Login";
 import Profile from "./Profile";
 import Body from "./Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App(){
     return(
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename="/">
     <Routes>
         <Route path="/"element={<Body/>} >
@@ -18,7 +21,7 @@ function App(){
     </Routes>
     </BrowserRouter>
 
-
+    </Provider>
     </>
 
     );
