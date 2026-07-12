240d77f60755ce2a2ca88bcd4e4de73beb5885f8
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-12 13:46:25 +0530

    Navigation for logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,26 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar =()=>{
   const user = useSelector(store=>store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async ()=>{
+    try{
+      await axios.post(BASE_URL+"/logout",{},{
+        withCredentials:true,
+      })
+      dispatch(removeUser());
+      return navigate("/login");
+
+    }catch(err){
+
+    }
+  }
 
 
     return(
@@ -32,7 +50,7 @@ const NavBar =()=>{
           </Link>
         </li>
         <li><Link to ="/settings">Settings</Link></li>
-        <li><Link to="/logout">Logout</Link></li>
+        <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
   </div>)}
