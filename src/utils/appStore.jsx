0ef440880ecf653c9b2f1a857fd0156f26aa73cd
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-16 17:29:53 +0530

    creatinf request reducers

--- src/utils/appStore.jsx
+++ src/utils/appStore.jsx
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
+import requestReducer from "./requestSlice"
 import connectionReducer from "./connectionSlice"
 const appStore = configureStore({
     reducer:{
         user:userReducer,
         feed:feedReducer,
         connection:connectionReducer,
+        requests:requestReducer,
+
     },
 })
 
